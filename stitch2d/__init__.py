656ce6a608c835d899125483a6ebde5b77a8c5b1
Elizabeth Wolkovich <adamfong888@gmail.com>
2024-07-26 16:06:46 -0700

    Adding stitching with numpy memory maps. Jupyter notebook for example

--- stitch2d/__init__.py
+++ stitch2d/__init__.py
@@ -1,6 +1,6 @@
 """Stitches a 2D grid of images into a mosaic"""
-from .mosaic import Mosaic, StructuredMosaic, create_mosaic, build_grid, is_grid
-from .tile import Tile, OpenCVTile, ScikitImageTile
+from .mosaic import Mosaic, StructuredMosaic, MemmapMosaic,  MemmapStructuredMosaic, create_mosaic, build_grid, is_grid
+from .tile import Tile, OpenCVTile, ScikitImageTile, MemmapTile, MemmapOpenCVTile
 
 
 __version__ = "1.2"
